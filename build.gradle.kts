4d47a2ab2d8127e1ab52a67ac70d4f4c1953ced8
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2022-11-09 13:34:12 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
-apply plugin: 'war'
+
 
 val developmentOnly by configurations.creating
 configurations {
